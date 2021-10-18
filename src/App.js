ef653f0e48d45e9da4a1b192f108faf9ad323fab
Andres Rodriguez <ancaro1487@gmail.com>
2021-10-17 22:38:06 -0500

    POC render chart

--- src/App.js
+++ src/App.js
@@ -1,9 +1,47 @@
+
+// React
+import React, {useState, useEffect} from 'react';
+
+// Components
+import Chart from './components/chart/Chart';
+
+// Styles
 import './App.css';
 
+
 function App() {
+
+  const [barsData, setBarsData] = useState({});
+  const [chartType, setChartType] = useState('bars');
+
+  const data = {
+    labels: [1.1, 2.7, 33],
+    // datasets is an array of objects where each object represents a set of data to display corresponding to the labels above. for brevity, we'll keep it at one object
+    datasets: [
+      {
+        label: 'Popularity of colours',
+        data: [55, -23, 96],
+        // you can set indiviual colors for each bar
+        backgroundColor: [
+          'red',
+          'green',
+          'blue',
+        ],
+        borderWidth: 1,
+      }
+    ]
+  }
+
+  useEffect(() => {
+    setBarsData(data);
+  }, [])
+
   return (
     <div className="App">
-      
+      <Chart 
+        barsData={barsData}
+        chartType={chartType}
+      />
     </div>
   );
 }
---
+++ src/components/chart/Chart.js
@@ -0,0 +1,61 @@
+
+// React
+import React from 'react';
+
+// ChartJS
+import { Bar, Scatter } from "react-chartjs-2";
+
+// Styles
+import './Chart.css'
+
+
+export const Chart = ({ barsData, chartType }) => {
+
+  return (
+    <div className="Chart">
+
+      {/* Bars Chart */}
+      {
+        chartType === 'bars' &&
+        <Bar
+          data={barsData}
+          options={{
+            plugins: {
+              title: {
+                display: true,
+                text: "Barras"
+              },
+              legend: {
+                display: true,
+                position: "bottom"
+             }
+            }
+          }}
+        />
+      }
+
+      {/* Scatter Chart */}
+      {
+        chartType === 'bars' &&
+        <Scatter
+          data={barsData}
+          options={{
+            plugins: {
+              title: {
+                display: true,
+                text: "Puntos"
+              },
+              legend: {
+                display: true,
+                position: "bottom"
+             }
+            }
+          }}
+        />
+      }
+
+    </div>
+  );
+}
+
+export default Chart;
